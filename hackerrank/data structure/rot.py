c024b9ef79ca9843c01ac795d179090d3866478b
root <root@HEMANTH>
2023-10-08 18:31:20 +0530

    from vsc_wslubuntu-terminal

--- hackerrank/data structure/rot.py
+++ hackerrank/data structure/rot.py
@@ -1,3 +1,5 @@
+"""
+--->>> EVEN THOUGH IT WORKS STILL IT GIVE RICE TO ERROR CALLED TIME EXECUTION <<<---
 def rot(arr,y,d):
    
     for i in range(d): 
@@ -19,4 +21,27 @@ if __name__ == "__main__":
     user_ip = input()
     arr = user_ip.split(" ")
     value = rot(arr,y,d)
+    print(*value)
+
+"""
+
+def rot(arr, y, d):
+    n = len(arr)
+    y %= n
+    result = [0] * n
+
+    for i in range(n):
+        result[(i - d + n) % n] = arr[i]
+
+    return result
+
+if __name__ == "__main__":
+    size_no_rot = input()
+    size_lst = size_no_rot.split(" ")
+    size_lst_int = [int(i) for i in size_lst]
+    y = size_lst_int[0]
+    d = size_lst_int[1]
+    user_ip = input()
+    arr = user_ip.split(" ")
+    value = rot(arr, y, d)
     print(*value)
